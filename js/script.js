3ba4964abc5758c6dd2138167ffefd8223f0f31b
codeurient <codeurient@gmail.com>
2021-12-30 11:30:13 +0200

    ready

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = function () {
+	document.body.classList.add('loaded');
   const tabs = document.querySelector(".tabs__buttons");
   const tabsContents = document.querySelectorAll(".tabs__content");
 
@@ -13,34 +14,54 @@ document.addEventListener("DOMContentLoaded", () => {
       tabsContents[currentBtn].classList.add("active");
     }
   });
-});
-
+};
 const nav = document.querySelector(".nav");
 const menuWrapper = document.querySelector(".menu-wrapper");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const modal = document.querySelectorAll(".sign-up-in");
-const modalWindow = document.querySelector(".modal");
-const btnClose = document.querySelector(".btn-close");
+const modalWindow = document.querySelectorAll(".modal");
+const btnClose = document.querySelectorAll(".btn-close");
 const acc = document.querySelector(".acc");
-const closeModal = () => {
-  modalWindow.classList.toggle("active");
-  document.body.classList.remove("active");
-};
-modalWindow.addEventListener("click", (evt) => {
-  if (!evt.target.closest(".modal__wrapper")) {
-    closeModal();
-  }
-});
-modal.forEach((item) => {
-  item.addEventListener("click", () => {
-    modalWindow.classList.toggle("active");
-    document.body.classList.add("active");
+
+const closeModals = () => {
+  modalWindow.forEach((item) => {
+    item.classList.remove("active");
+    document.body.classList.remove("active");
   });
-});
-btnClose.addEventListener("click", () => {
-  closeModal();
-});
+};
+
+modal.forEach((item) =>
+  item.addEventListener("click", () => {
+    modalWindow[0].classList.add("active");
+    document.body.classList.add("active");
+  })
+);
+
+btnClose.forEach((item) =>
+  item.addEventListener("click", () => {
+    closeModals();
+  })
+);
+
 menuWrapper.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("animate");
   nav.classList.toggle("active");
 });
+
+const videos = [
+  '<iframe width="80%" height="80%" src="https://www.youtube.com/embed/BqPoCq4Y0yk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+  '<iframe width="80%" height="80%" src="https://www.youtube.com/embed/z8uNmQaZgMY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+];
+const video = document.querySelectorAll(".social__grid-item.video");
+const here = document.querySelector(".video-iframe");
+const modalV = document.querySelector(".modal-v");
+
+video.forEach((item) =>
+  item.addEventListener("click", () => {
+    modalWindow[1].classList.add("active");
+    const src = item.dataset.video;
+    here.innerHTML = "";
+    here.innerHTML = videos[src];
+    document.body.classList.add("active");
+  })
+);
